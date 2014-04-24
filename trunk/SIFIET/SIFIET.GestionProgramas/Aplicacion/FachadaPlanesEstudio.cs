3fbeea845cdd23540e2f37e7afe99137874387da
aempsax@gmail.com <aempsax@gmail.com@8e972253-bc44-0288-dcbe-bccd988e1131>
2014-04-24 22:08:38 +0000

    cambios, vista, servicios, fachadas, CRUD ASIGNATURA

--- trunk/SIFIET/SIFIET.GestionProgramas/Aplicacion/FachadaPlanesEstudio.cs
+++ trunk/SIFIET/SIFIET.GestionProgramas/Aplicacion/FachadaPlanesEstudio.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SIFIET.GestionProgramas.Datos.Modelo;
+using SIFIET.GestionProgramas.Dominio.Servicios;
 
 namespace SIFIET.GestionProgramas.Aplicacion
 {
     public static class FachadaPlanesEstudio
     {
+        public static List<PLANESTUDIO> ConsultarPlanesEstudios(string palabraBusqueda)
+        {
+            return ServicioPlanesEstudio.ConsultarPlanestudios(palabraBusqueda);
+        }
+
     }
 }
--- trunk/SIFIET/SIFIET.GestionProgramas/Dominio/Servicios/ServicioPlanesEstudio.cs
+++ trunk/SIFIET/SIFIET.GestionProgramas/Dominio/Servicios/ServicioPlanesEstudio.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SIFIET.GestionProgramas.Datos.Modelo;
 
 namespace SIFIET.GestionProgramas.Dominio.Servicios
 {
     static class ServicioPlanesEstudio
     {
+        public static List<PLANESTUDIO> ConsultarPlanestudios(string palabraBusqueda)
+        {
+            try
+            {
+                var db = new GestionProgramasEntities();
+                List<PLANESTUDIO> lista = new List<PLANESTUDIO>();
+                if (String.IsNullOrEmpty(palabraBusqueda))
+                {
+                    lista = (from e in db.PLANESTUDIOS select e).ToList();
+                    return lista;
+                }
+                else
+                {
+                    lista = (from e in db.PLANESTUDIOS
+                             where
+                                 (e.NOMBREPLANESTUDIOS.Contains(palabraBusqueda) |
+                                  e.IDENTIFICADORPLANESTUDIOS == decimal.Parse(palabraBusqueda))
+                             select e).ToList();
+                    return lista;
+
+                }
+
+            }
+            catch (Exception)
+            {
+                return new List<PLANESTUDIO>();
+            }
+        }
     }
 }
