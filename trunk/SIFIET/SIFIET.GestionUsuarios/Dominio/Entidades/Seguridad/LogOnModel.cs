9ed7c3cbbd1e3167d56e9be529f0abf024a88d7e
aempsax@gmail.com <aempsax@gmail.com@8e972253-bc44-0288-dcbe-bccd988e1131>
2014-06-07 22:00:04 +0000

    Autentificacion, el control de acceso esta para el gestionar Asignaturas , se debe agregar las etiquetas en los metodos controlador de todos los crud

---
+++ trunk/SIFIET/SIFIET.GestionUsuarios/Dominio/Entidades/Seguridad/LogOnModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+ 
+
+namespace SIFIET.GestionUsuarios.Dominio.Entidades.Seguridad
+{
+    public class LogOnModel
+    {
+        [Required]
+        [RegularExpression("^([a-zA-Z0-9 .&'-]+)$", ErrorMessage = "Caracteres no validos")]
+        [DisplayName("Usuario")]
+        public string UserName { get; set; }
+ 
+        [Required]
+        [DataType(DataType.Password)]
+        [DisplayName("Contraseña")]
+        public string Password { get; set; }
+ 
+        [DisplayName("Recordarme?")]
+        public bool RememberMe { get; set; }
+    }
+
+}
+
---
+++ trunk/SIFIET/SIFIET.GestionUsuarios/Dominio/Entidades/Seguridad/ProveedorRoles.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Security;
+using SIFIET.GestionUsuarios.Datos.Modelo;
+
+namespace SIFIET.GestionUsuarios.Dominio.Entidades.Seguridad
+{
+    public class ProveedorRoles : RoleProvider 
+    {
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ApplicationName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+            var db = new GestionUsuariosEntities();
+            username += "@unicauca.edu.co";
+            var usuario = (from e in db.USUARIOs where e.EMAILINSTITUCIONALUSUARIO.Equals(username) select e).FirstOrDefault();
+            if (usuario == null) return new string[] {};
+            var roles = usuario.ROLs.Select(rol => rol).ToList();
+            var permisosRoles = roles.Select(rol => rol.PERMISOS).ToList();
+            return (from permisoRol in permisosRoles from permiso in permisoRol select permiso.NOMBREPERMISO).ToArray();
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            var db = new GestionUsuariosEntities();
+            username += "@unicauca.edu.co";
+            var usuario = (from e in db.USUARIOs where e.EMAILINSTITUCIONALUSUARIO.Equals(username) select e).FirstOrDefault();
+            if (usuario == null) return false;
+            var roles = usuario.ROLs.Select(rol => rol).ToList();
+            /*for (int i = 0; i < roles.Count; i++)
+            {
+                var rol = roles.ElementAt(i).Trim();
+                roles.RemoveAt(i);
+                roles.Add(rol);
+            }
+            return roles.Any(r => r.Equals(roleName, StringComparison.CurrentCultureIgnoreCase));
+            */
+            var permisosRoles = roles.Select(rol => rol.PERMISOS).ToList();
+            var nombrespermisos =(from permisoRol in permisosRoles from permiso in permisoRol select permiso.NOMBREPERMISO).ToArray();
+            return nombrespermisos.Any(r => r.Equals(roleName, StringComparison.CurrentCultureIgnoreCase));
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
